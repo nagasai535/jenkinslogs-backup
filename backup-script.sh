a846a19fe165ca8f31b19e66bc2361d307c6d360
root <root@ip-172-31-26-183.ap-south-1.compute.internal>
2021-08-14 15:45:50 +0000

    Nagasai Jenkins File

--- backup-script.sh
+++ backup-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DATE=$(date +%S-%M-%H-%d-%m-%Y)
-BACKUP_DIR=/var/log/backup
+BACKUP_DIR=/var/log/backup-script
 
-tar -czvf $BACKUP_DIR/backupscript-repo-$DATE.tar.gz /var/log/jenkins
+tar -czvf $BACKUP_DIR/jenkins-home-$DATE.tar.gz /var/log/jenkins
